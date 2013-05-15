3ba47403025fadb994a92a3cde3eb0b6cfa64f95
rakwaht <Martintonid@gmail.com>
2013-05-15 19:17:23 +0200

    dio animale

--- prog2.cc
+++ prog2.cc
@@ -2,21 +2,28 @@ using namespace std;
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<utility>
 #include<queue>
 #include<cstring>
+#include <algorithm>
 
 int N,M,T;
 string * dati;
 int ** tab;
 int ** m;
+vector < pair<int,char> > v;
 
 int check_row(int indice,int t);
-int recursive(string s,int i,int j,char v,int t);
+int recursive(int i, int j, char c);
 int sol();
 int calcola(int,int);
+void raggruppa(string s);
+
 int main(){
 	ifstream in("input.txt");
+	ofstream out("output.txt");
 	in >> N >> M >> T;
+	T += 1;
 	dati = new string [N];	
 	tab = new int * [N];
 	for(int i=0; i<N; i++){
@@ -26,57 +33,49 @@ int main(){
 		in >> dati[i];
 	}
 	
-	
-	sol();
-	ofstream out("output.txt");
-	out << tab[N-1][T-1];
-	return 0;
-}
+	//sol();
+	cout << check_row(0,0) << endl;
+	cout << check_row(1,0) << endl;
+	cout << check_row(2,0) << endl;
+	cout << check_row(3,0) << endl;
+	cout << check_row(4,0) << endl;
+	/*for(int i=0; i<N; i++){
+		for(int j=0; j<T; j++){
+			cout << tab[i][j] << " " ;
+		}
+		cout << endl;
+	}*/
 
-void print(){
-	for(int i=0; i<N; i++){
-		cout << dati[i] << endl;
-	}
+	out << tab[N-1][T-1];
+	//cout << tab[N-1][T-1];
+	return 0;
 }
 
 int check_row(int indice,int t){
 	int cont=0;
-	m = new int * [M];
-	for(int i=0; i<M; i++){
+	v.clear();
+	raggruppa(dati[indice]);
+	m = new int * [v.size()];
+	for(int i=0; i<v.size(); i++){
 		m[i]= new int [t+1];
 	}
-	for(int i=0; i<M; i++){
+	for(int i=0; i<v.size(); i++){
 		for(int j=0; j<=t; j++){
 			m[i][j]=-1;
 		}
 	}
-	int temp = recursive(dati[indice],0,0,'a',t);
-	for(int i=0; i<M;i++){
+	int temp = recursive(v.size()-1,t,'a');	
+	for(int i=0; i<v.size(); i++){
+		for(int j=0; j<=t; j++){
+			cout << m[i][j] << " " ;
+		}
+		cout << endl;
+	}	
+	for(int i=0; i<v.size();i++){
 		delete m[i];
 	}
 	delete m;
-	return temp;
-}
-
-int recursive(string s,int i,int j,char v,int t){
-	if(j==t+1){
-		return -1;
-	}
-	else if(i==M || j>t){
-		return 0;
-	}
-	else if(m[i][j] != -1){
-		if(s[i]==v) return m[i][j]+1;
-		else return m[i][j];
-	}
-	else if(s[i]==v){
-		m[i][j] = recursive(s,i+1,j,v,t) + 1;
-		return m[i][j];
-	}
-	else{
-		m[i][j] = max(recursive(s,i+1,j,v,t),recursive(s,i+1,j+1,s[i],t)+1);
-		return m[i][j];
-	}
+	return 0;
 }
 
 int sol(){
@@ -102,6 +101,49 @@ int calcola(int i, int j){
 		if(temp>max)max = temp;
 		indice -= 1;
 	}
-	cout << i << " " << j << " " << max << endl;
+	//cout << i << " " << j << " " << max << endl;
 	return max;
+}
+
+int recursive(int i, int j, char c){
+	if(i < 0 || j < 0 ){
+		return 0;
+	}
+	else{
+		if( m[i][j] != -1 ){
+			return m[i][j];
+		}
+		else{
+			if( v[i].second == c ){
+				m[i][j] = v[i].first + recursive(i-1,j,c);
+				return m[i][j];
+			}
+			else{
+			   m[i][j] = max(v[i].first + recursive(i-1,j-1,v[i].second),recursive(i-1,j,c));
+			   return m[i][j];
+			}
+		}
+	}
+}
+
+void raggruppa(string s){
+	char last=s[0];
+	int cont = 0;
+	for(int i=0; i<M; i++){
+		if(s[i]==last){
+			cont++;
+		}
+		else{	
+			pair <int,char> p;
+			p.first = cont;
+			p.second = last;
+			v.push_back(p);
+			cont = 1;
+			last =s[i];
+		}
+	}
+	pair <int,char> p;
+	p.first = cont;
+	p.second = last;
+	v.push_back(p);
 }
